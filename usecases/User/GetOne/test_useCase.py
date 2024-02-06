b6f8d2722cbb603d1fa9a8601e1697fa038bc252
felipegenef <felipegenef@gmail.com>
2024-02-06 17:11:38 -0300

    add better names for tests

--- usecases/User/GetOne/test_useCase.py
+++ usecases/User/GetOne/test_useCase.py
@@ -1,33 +1,32 @@
 import unittest
 from unittest.mock import Mock
 from usecases.User.GetOne.Service import GetOneUserService
-
+from json import dumps,loads
 
 class TestGetOneUser(unittest.TestCase):
-    def test_shoudGetAnUser(self):
-        repositoryMock = Mock()
-        service = GetOneUserService(repositoryMock)
+    def setUp(self):
+        self.repositoryMock=Mock()
+    def test_return_user_when_found(self):
+        service = GetOneUserService(self.repositoryMock)
 
         # Mock repository method to return some data
-        repositoryMock.findOne.return_value = {"id": "123", "name": "John"}
+        self.repositoryMock.findOne.return_value = {"id": "123", "name": "John"}
 
         # Call the execute method
         result = service.execute("123")
 
         # Assert that the result is what we expect
         self.assertEqual(result, {"id": "123", "name": "John"})
-    def test_shoudNotGetAnUser(self):
-        repositoryMock = Mock()
-        service = GetOneUserService(repositoryMock)
+    def test_return_none_when_not_found(self):
+        service = GetOneUserService(self.repositoryMock)
 
         # Mock repository method to return some data
-        repositoryMock.findOne.return_value = {"id": "123", "name": "John"}
+        self.repositoryMock.findOne.return_value = None
 
         # Call the execute method
         result = service.execute("123")
 
-        # Assert that the result is what we expect
-        self.assertNotEqual(result, {"id": "122", "name": "John"})
+        self.assertIsNone(result)
 
 if __name__ == '__main__':
     unittest.main()
